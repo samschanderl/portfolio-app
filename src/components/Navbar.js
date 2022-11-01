f8a83f03d7387c8c37828982bbfae21db0f9e2aa
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-11-01 11:25:10 +0100

    changed logo & favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsGithub, BsLinkedin} from 'react-icons/bs';
+import logo from '../images/logo-samuel-transparent.png'
 
 // get context
 import { useStateContext } from '../contexts/StateContext';
@@ -13,9 +14,10 @@ export default function Navbar() {
   return (
     <div className="fixed bg-gradient-to-r from-slate-600 to-slate-800 w-full top-0 z-50 p-4 flex justify-stretch shadow-md shadow-slate-400">
       <div className="container max-w-screen-lg flex gap-5 justify-end items-center mx-auto">
-        <div className="mr-auto text-left">
-          <p>Samuel</p>
-          <p>Schanderl</p>
+        <div className="mr-auto text-left grid grid-cols-2 md:px-6">
+          <img className="w-12 row-span-2 content-center gap-0" src={logo}/>
+          <p className="md:text-md text-sm">Samuel</p>
+          <p className="md:text-md text-sm">Schanderl</p>
         </div>
         <a href={GithubURL} className="hover:cursor-pointer"><BsGithub className="text-2xl"/></a>
         <a href={LinkedinURL} className="hover:cursor-pointer"><BsLinkedin className="text-2xl"/></a>
